a0ee7c67ab30080e12dfeaa056f56ceeb0680f2f
Ivan <grecigor11@gmail.com>
2023-02-02 21:10:00 +0300

    added tests for board

---
+++ tests/gameplay/board/test_board_3.py
@@ -0,0 +1,40 @@
+# content of test_board_3.py
+# Positive test
+
+
+from pytest_bdd import scenario, given, then
+
+from models.pause import Pause
+
+
+@scenario('board.feature', 'Checking the end of the game after the entire game board is full')
+def test_board_3():
+    pass
+
+
+
+@given("The game is in progress", target_fixture="game_in_progress")
+def game_in_progress():
+    pause = Pause()
+    assert pause.active_pause == True
+    return {"game_progress": True}
+
+
+@given("Fully filled board with houses of different levels", target_fixture="full_board")
+def full_board():
+    return [
+        [1, 2, 10, 9, 1, 5],
+        [2, 3, 9, 10, 2, 1],
+        [3, 4, 8, 9, 3, 2],
+        [4, 5, 6, 5, 4, 5],
+        [5, 6, 5, 4, 6, 7],
+        [6, 7, 4, 3, 2, 1],
+
+    ]
+
+
+@then("Game is stopped")
+def step_impl(full_board):
+    for i in full_board:
+        for j in i:
+            assert j != 0
